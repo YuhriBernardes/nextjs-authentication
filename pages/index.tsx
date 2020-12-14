3e1ccb614ba1c6c3a34c6b07323711f27cb6cbf4
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2020-12-14 19:46:55 -0300

    initial project structure

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import { FC } from 'react'
+
+const Home: FC = () => (
+  <div>
+    <Head>
+      <title>Create Next App</title>
+    </Head>
+    <main>
+      <p>Hello world</p>
+    </main>
+  </div>
+)
+export default Home
